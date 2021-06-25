70bf2e89e53e5e2183c299320b9e8eea0edcd891
brittmoore86 <brittmoore86@gmail.com>
2021-06-25 14:22:49 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// API key: ce57e369227af8466e0b1a3e66b17f67
+
+// Home page
+    // Data display, success page (weather data)
+    // Error page if failed (wrong input or server is down edge cases)
+
+// JS takes in user data
+    // Uses data for API call
+    // Return data and parse info for UI
+
+
+class Fetch {
+    async getCurrent(input) {
+        const myKey = "ce57e369227af8466e0b1a3e66b17f67";
+    }
+}
+
+const response = await fetch(
+    
+)
